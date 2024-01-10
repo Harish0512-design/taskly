362efc7901ca332da7954ee585b166cc4633559f
Harish <harishsomsole@gmail.com>
2024-01-11 02:10:44 +0530

    Rendering data to the templates and using if statement and for loop in templates

--- todo/views.py
+++ todo/views.py
@@ -9,9 +9,33 @@ def register(request):
 
 
 def login(request):
+    users_details = [
+        {
+            "id": 1,
+            "name": "Harish",
+            "Profession": "Software Engineer"
+        },
+        {
+            "id": 2,
+            "name": "Harish2",
+            "Profession": "Software Engineer"
+        },
+        {
+            "id": 3,
+            "name": "Harish3",
+            "Profession": "Software Engineer"
+        }
+    ]
+
+    context = {"users": users_details}
+
     # return HttpResponse("Login page")
-    return render(request, "todo/login.html")
+    return render(request, "todo/login.html", context=context)
 
 
 def home(request):
-    return render(request, "todo/index.html")
+    context = {
+        "first_name": "Harish",
+        "last_name": "Somsole"
+    }
+    return render(request, "todo/index.html", context=context)
