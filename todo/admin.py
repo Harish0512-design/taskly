c596acf75789b6608f418fe8b73025db09c8c63c
Harish <harishsomsole@gmail.com>
2024-01-11 11:51:17 +0530

    Admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from todo.models import Task
+
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    list_filter = ("date_posted", "date_updated")
+    list_editable = ("title", "content")
+    list_per_page = 10
+
+    def get_list_editable(self, request):
+        if request.user.is_superuser:
+            return self.list_editable
+        else:
+            return ()  # returning empty tuple so that no fields are editable
+
+
+admin.site.register(Task, TaskAdmin)
