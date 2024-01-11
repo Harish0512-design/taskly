33a4dc872806676838a82a262426a0296d75eae8
Harish <harishsomsole@gmail.com>
2024-01-11 16:12:31 +0530

    ORM 2: Qobjects AND, OR, NOT conditions

---
+++ todo/orm_queries.py
@@ -0,0 +1,72 @@
+
+# tasks_queryset = Task.objects.all()
+
+# queryset : set of objects in db
+
+# <QuerySet [<Task: Complete Django- # 1>, <Task: Environments- # 2>, <Task: Docker- # 3>, <Task: Kubernates- # 4>, <Task: Django Rest Framework- # 5>, <Task: Jwt Token- # 6>]>
+
+# single_task_obj = Task.objects.get(id=3)
+# <Task: Docker- # 3>
+
+# __contains like this : called as FieldLookups
+
+# filtered_queryset = Task.objects.filter(content__contains = "Django")
+# filtered_queryset = Task.objects.filter(content__icontains = "Django")
+
+# filtered_queryset = Task.objects.filter(content__startswith = "Django")
+# filtered_queryset = Task.objects.filter(content__istartswith = "Django")
+
+# filtered_queryset = Task.objects.filter(content__endswith = "Django")
+# filtered_queryset = Task.objects.filter(content__iendswith = "Django")
+
+# filtered_queryset = Task.objects.filter(id__in=[3,4,5])
+
+# filtered_queryset = Task.objects.filter(id__gt = 2)
+# filtered_queryset = Task.objects.filter(id__gte = 2)
+
+# filtered_queryset = Task.objects.filter(id__lt = 2)
+# filtered_queryset = Task.objects.filter(id__lte = 2)
+
+# filtered_queryset = Review.objects.filter(date_posted__isnull=True)
+
+# filtered_queryset = Task.objects.filter(date_posted__year = 2024)
+# filtered_queryset = Task.objects.filter(date_posted__month = 1)
+# filtered_queryset = Task.objects.filter(date_posted__day = 11)
+
+# filtered_queryset = Task.objects.filter(date_posted__date = datetime.date(2024, 1, 11))
+# filtered_queryset = Task.objects.filter(date_posted__date_gt = datetime.date(2024, 1, 11))
+
+# filtered_queryset = Task.objects.filter(date_posted__hour = 3)
+# filtered_queryset = Task.objects.filter(date_posted__minute = 3)
+# filtered_queryset = Task.objects.filter(date_posted__second = 3)
+
+
+# FieldLookUps on single object
+
+# queryset_obj = Task.objects.get(id__exact=14)
+# queryset_obj = Task.objects.get(title__iexact="Django")
+
+# queryset_obj = Review.objects.get(task__title__contains = "Django")
+
+
+# queryset_obj = Task.objects.get(title__regex=r'^(An?|The) +')
+# queryset_obj = Task.objects.get(title__iregex=r'^(An?|The) +')
+
+
+# Note:
+# query_obj_address = single_task_obj.query (or) tasks_queryset.query
+# sql_limit_time = connection.queries
+# o/p: [{'sql': 'SELECT "todo_task"."id", "todo_task"."date_updated", "todo_task"."title", "todo_task"."content", "todo_task"."date_posted" FROM "todo_task" WHERE "todo_task"."id" = 2 LIMIT 21', 'time': '0.016'}]
+
+
+
+# IF I want to perform Complex LookUps or condition checking then use 'Q' Objects
+
+# OR:
+# Task.objects.get(Q(id__exact = 1) | Q(title__iexact = "django"))
+
+# AND:
+# Task.objects.get(Q(id__exact = 1) & Q(title__iexact = "django"))
+
+# NOT:
+# Task.objects.filter(~Q(id__exact = 1))
