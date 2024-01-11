89913aaac22113bb8970dcc6b513bd3163b9df99
Harish <harishsomsole@gmail.com>
2024-01-11 09:20:47 +0530

    models and makemigrations,migrate commands

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TimeStampModel(models.Model):
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Task(TimeStampModel):
+    title = models.CharField(max_length=85)
+    content = models.CharField(max_length=300)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
